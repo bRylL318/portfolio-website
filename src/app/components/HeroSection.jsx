8c0fdf45f20eef14099e7d2d738048b0fefde0a4
bRylL318 <eldrin.web2022@gmail.com>
2023-12-23 20:36:00 +0800

    firstCommit

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,56 @@
+"use client";
+import React from 'react';
+import Image from "next/image";
+import { TypeAnimation } from 'react-type-animation';
+export const HeroSection = () => {
+  return (
+    <section>
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-teal-600">
+            Hello, I'm{""} 
+            </span>
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                "Eldrin",
+                1000,
+                "DevOps Engineer",
+                1000,
+                "Software Engineer",
+                900
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+                      
+          </h1>
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+          To join your highly reputable company that will enhance my skills and potentials and will broaden my knowledge that the business world has to offer in preparing myself to the professional and globally competitive
+          </p>
+          <div>
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-teal-500 via-blue-500 to-teal-500 hover:bg-slate-200 text-white">Hire Me</button>
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-teal-500 via-blue-500  to-teal-500 hover:bg-slate-800 text-white mt-3">
+              
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px5 py-2">Download CV</span>
+              </button>
+          </div>
+        </div>
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+          <div className="rounded-full bg-[#52D3D8] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image 
+            src={"/images/eldrinPic-removebg.png"}
+            alt="hero image"
+            className="rounded-full absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+            width={4000}
+            height={350}
+            />
+           </div>
+
+        </div>
+      </div>
+    </section>
+  )
+}
